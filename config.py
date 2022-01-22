d9644477b25b08e39a512bd0208cb1745a8fe593
minsikmoon <msmoon.business@gmail.com>
2022-01-22 19:01:27 +0900

    ..

--- config.py
+++ config.py
@@ -3,4 +3,4 @@ import os
 BASE_DIR = os.path.dirname(__file__)
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(BASE_DIR, 'pybo.db'))
-SQLALCHEMY_TRACK_MODIFICATIONS = false
+SQLALCHEMY_TRACK_MODIFICATIONS = False
