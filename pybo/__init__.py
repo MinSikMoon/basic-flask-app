9ae038917464c7a98199c9184b2e43340ca9f5a3
minsikmoon <msmoon.business@gmail.com>
2022-01-22 18:34:19 +0900

    app에 blueprint앱 등록

--- pybo/__init__.py
+++ pybo/__init__.py
@@ -1,9 +1,7 @@
 from flask import Flask
-app = Flask(__name__)
 
-def create_app():
-    app = Flask(__name__)
-    @app.route('/')
-    def hello_pybo():
-        return 'hello python'
-    return app
+def create_app(): #flask의 내장함수, 첫시작
+    app = Flask(__name__) #flask앱인 app을 만든다. 
+    from .views import main_views #views 디렉터리의 main_views 코드를 import
+    app.register_blueprint(main_views.bp) #app(flask앱객체)에 블루 프린트 객체인 bp를 등록한다. 
+    return app #app을 return한다. 
